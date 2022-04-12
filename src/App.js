6544204be592bc17a5adbde9d9c834b60e641cc3
Alan Shirk <ashirk94@gmail.com>
2022-04-11 23:59:46 -0700

    layout component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
-//import Navbar from './components/navbar.component';
-import Layout from './components/layout.component';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
