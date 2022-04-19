89a9c8d1b0c427d67ed3d8d7098a6957f50f2c67
Alan Shirk <ashirk94@gmail.com>
2022-04-18 22:38:42 -0700

    sidebar polished

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,9 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
-import Logo from '../../assets/images/laptop-computer-logo.png'
+import { Link, NavLink } from 'react-router-dom'
+import Logo from '../../assets/images/knight2.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -8,6 +11,45 @@ const Sidebar = () => {
       <Link className="logo" to="/">
         <img src={Logo} alt="logo" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="darkred" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="blue" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="white" />
+        </NavLink>
+        <br />
+        <br />
+        <br />
+        <br />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/alan-shirk/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="lightblue" />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/alan-shirk/"
+        >
+          <FontAwesomeIcon icon={faGithub} color="white" />
+        </a>
+      </nav>
     </div>
   )
 }
