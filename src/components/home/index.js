11afc9732d59a9537752c420f125a6677ebc0fcf
Alan Shirk <ashirk94@gmail.com>
2022-06-18 22:25:25 -0700

    animate image

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -14,8 +14,7 @@ const Home = () => {
           <h1>I'm <span className="name">Alan Shirk.</span></h1>
         </span>
         <div className="delay2">
-          <h2>I'm a Full Stack Web Developer</h2>
-          <h2>from Eugene, Oregon.</h2>
+          <h2>I'm a Full Stack JavaScript and C# Developer from Eugene, Oregon.</h2>
         </div>
         <Link to="/contact" className="contact-link">
           Contact Me
