99deb04bf8a3258c33b1a5035642b23d1c6c210b
Alan Shirk <ashirk94@gmail.com>
2022-06-20 22:52:06 -0700

    font swap

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div>
 		<div className='text-zone'>
 			<h2>Contact Me</h2>
-            <p className='text'>If you would be interested in working with me, send an email using the form below:</p>
+            <p className='text'>If you would be interested in working with me,<br/>please send an email using the form below:</p>
             <div className="contact-form">
                 <form>
                     <ul>
