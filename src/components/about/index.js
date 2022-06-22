8addf63f5091f07d8d7d432dfc075a08745c7e23
Alan Shirk <ashirk94@gmail.com>
2022-06-22 09:28:05 -0700

    touchups for mobile

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,7 +7,7 @@ const About = () => {
 		<div className='text-zone'>
 			<h2>About Me</h2>
             <div className='container'>
-            <p className='text'>I'm Alan, a passionate and enthusiastic junior developer. While working on my AAS degree I focused on .NET Core development in C#. To complete the degree I was an intern at Oregon Criminal Defense Lawyers Association where I was mostly writing front-end JavaScript code.
+            <p className='text'>I'm Alan, a passionate and enthusiastic junior developer. While working on my AAS degree I focused on .NET Core development in C#. To complete the degree I became a web development intern at Oregon Criminal Defense Lawyers Association where I was mostly writing front-end JavaScript code.
             <br/><br/>
             I have deployed several different applications over the past two years, demonstrating my ability to code a project from start to finish and my understanding of the software development life-cycle. Check out my GitHub to see what I've been building so far!
             <br/><br/>
