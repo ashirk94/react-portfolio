ac4b361541265df59f5680689370b131af10e6a0
Alan Shirk <ashirk94@gmail.com>
2022-08-01 13:41:21 -0700

    portfolio page flexbox

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -8,13 +8,15 @@ const Portfolio = () => {
         <div>
 		<div className='text-zone'>
 			<h2>Projects</h2>
-            <a className="links large" href='http://channelthis.tv' target="_blank" rel="noreferrer">ChannelThis.tv</a>
+            <div className='link-zone'>
+            <a className="links" href='http://channelthis.tv' target="_blank" rel="noreferrer">ChannelThis.tv</a>
             {/* <img className="image" src={channelThis} alt="channelthis"></img> */}
-            <a className="links" href='https://github.com/ashirk94/RentTech' target="_blank" rel="noreferrer">RentTech</a><br/>
+            <a className="links" href='https://github.com/ashirk94/RentTech' target="_blank" rel="noreferrer">RentTech</a>
             <a className="links" href='https://github.com/ashirk94/MyGameList' target="_blank" rel="noreferrer">MyGameList</a>
-            <a className="links" href='https://github.com/ashirk94/custom-google-map' target="_blank" rel="noreferrer">custom-google-map</a><br/>
+            <a className="links" href='https://github.com/ashirk94/custom-google-map' target="_blank" rel="noreferrer">custom-google-map</a>
             <a className="links" href='https://github.com/ashirk94/ScandinavianFood' target="_blank" rel="noreferrer">ScandinavianFood</a>
-            <a className="links" href='https://github.com/ashirk94/react-portfolio' target="_blank" rel="noreferrer">alanshirk.com</a><br/>        
+            <a className="links" href='https://github.com/ashirk94/react-portfolio' target="_blank" rel="noreferrer">alanshirk.com</a>    
+            </div>   
 		</div>
 		{/* <Loader type='ball-pulse' /> */}
 	</div>
