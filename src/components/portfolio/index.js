29f350bdbfa2bb1e842182bfb38fefaa6d6bf85a
Alan Shirk <ashirk94@gmail.com>
2022-08-02 18:29:20 -0700

    azure sites redeployed

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -11,10 +11,10 @@ const Portfolio = () => {
             <div className='link-zone'>
             <a className="links" href='http://channelthis.tv' target="_blank" rel="noreferrer">ChannelThis.tv</a>
             {/* <img className="image" src={channelThis} alt="channelthis"></img> */}
-            <a className="links" href='https://github.com/ashirk94/RentTech' target="_blank" rel="noreferrer">RentTech</a>
+            <a className="links" href='https://tech-rental.azurewebsites.net/' target="_blank" rel="noreferrer">RentTech</a>
             <a className="links" href='https://github.com/ashirk94/MyGameList' target="_blank" rel="noreferrer">MyGameList</a>
             <a className="links" href='https://github.com/ashirk94/custom-google-map' target="_blank" rel="noreferrer">custom-google-map</a>
-            <a className="links" href='https://github.com/ashirk94/ScandinavianFood' target="_blank" rel="noreferrer">ScandinavianFood</a>
+            <a className="links" href='https://scandinavian-food.azurewebsites.net/' target="_blank" rel="noreferrer">ScandinavianFood</a>
             <a className="links" href='https://github.com/ashirk94/react-portfolio' target="_blank" rel="noreferrer">alanshirk.com</a>    
             </div>   
 		</div>
