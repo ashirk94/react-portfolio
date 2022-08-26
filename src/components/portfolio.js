e3e158534451cdb68c20b0fdae6659c9020a982b
Alan Shirk <ashirk94@gmail.com>
2022-08-26 13:14:47 -0700

    Added produce shop project

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,29 +1,70 @@
-import myGameList from "../assets/images/mygamelist.jpg"
-import rentTech from "../assets/images/renttech.jpg"
-import scandinavianFood from "../assets/images/scandinavianfood.jpg"
+import myGameList from '../assets/images/mygamelist.jpg'
+import rentTech from '../assets/images/renttech.jpg'
+import scandinavianFood from '../assets/images/scandinavianfood.jpg'
+import produceShop from '../assets/images/produceShop.jpg'
 
 const Portfolio = () => {
-    return (
-        <div>
-		<div className='portfolio'>
-			<h2>Projects</h2>
-            <div className='link-zone'>
-                <div className='item'>
-                <a className="links" href='https://tech-rental.azurewebsites.net/' target="_blank" rel="noreferrer">RentTech</a>
-            <img className="image" src={rentTech} alt="RentTech"></img>
-                </div>       
-            <div className='item'>
-            <a className="links" href='http://mygamelist-env.eba-pjfkzxaf.us-west-2.elasticbeanstalk.com/' target="_blank" rel="noreferrer">MyGameList</a>
-            <img className="image" src={myGameList} alt="MyGameList"></img>
-            </div>
-            <div className='item'>
-            <a className="links" href='https://scandinavian-food.azurewebsites.net/' target="_blank" rel="noreferrer">ScandinavianFood</a> 
-            <img className="image" src={scandinavianFood} alt="ScandinavianFood"></img>
-            </div>   
-            </div>
+	return (
+		<div>
+			<div className='portfolio'>
+				<h2>Projects</h2>
+				<div className='link-zone'>
+					<div className='item'>
+						<a
+							className='links'
+							href='https://main.d29j317vl7nv5o.amplifyapp.com/'
+							target='_blank'
+							rel='noreferrer'>
+							Produce Shop
+						</a>
+						<img
+							className='image'
+							src={produceShop}
+							alt='Produce Shop'></img>
+					</div>
+					<div className='item'>
+						<a
+							className='links'
+							href='https://tech-rental.azurewebsites.net/'
+							target='_blank'
+							rel='noreferrer'>
+							RentTech
+						</a>
+						<img
+							className='image'
+							src={rentTech}
+							alt='RentTech'></img>
+					</div>
+					<div className='item'>
+						<a
+							className='links'
+							href='http://mygamelist-env.eba-pjfkzxaf.us-west-2.elasticbeanstalk.com/'
+							target='_blank'
+							rel='noreferrer'>
+							MyGameList
+						</a>
+						<img
+							className='image'
+							src={myGameList}
+							alt='MyGameList'></img>
+					</div>
+					<div className='item'>
+						<a
+							className='links'
+							href='https://scandinavian-food.azurewebsites.net/'
+							target='_blank'
+							rel='noreferrer'>
+							ScandinavianFood
+						</a>
+						<img
+							className='image'
+							src={scandinavianFood}
+							alt='ScandinavianFood'></img>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-    )
+	)
 }
 
 export default Portfolio
