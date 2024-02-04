89b1618429e9cef8c864d5a1b612b91b7d54b0de
Alan Shirk <ashirk94@gmail.com>
2024-02-04 02:16:35 -0800

    Trimmed about section

--- src/components/about.js
+++ src/components/about.js
@@ -3,11 +3,10 @@ const About = () => {
         <div>
 		<div className='about'>
 			<h2>About Me</h2>
+            <br/><br/>
             <div className='container'>
-            <p className='text'>I am a Software Engineer with over 4 years of experience writing code and building web applications. Currently I am a Junior in Computer Science at Portland State University. In 2022 I graduated from Lane Community College with an AAS in Computer Programming. During the final term of that degree I became a Web Development Intern at OCDLA, the Oregon Criminal Defense Lawyers Association.
-            <br/><br/>
-            To see some of my work, check out the projects page. Bloom is my largest project, and I continue to add updates to it occasionally.
-            <br/><br/>
+            <p className='text'>I am a Software Engineer with over 4 years of programming experience. Currently I am a Junior in Computer Science at Portland State University. In 2022 I graduated from Lane Community College with an AAS in Computer Programming. During the final term of that degree I was a Web Development Intern at OCDLA, the Oregon Criminal Defense Lawyers Association.
+            <br/><br/><br/>
             <a className='links' href='/pdfs/AlanShirkResume.pdf' target='_blank'>Click here to view my resumé!</a></p>
             </div>
 		</div>
