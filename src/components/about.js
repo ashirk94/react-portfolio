6c3aa099462b29b1eecbaf8fe315811f63f45a16
Alan Shirk <ashirk94@gmail.com>
2025-07-01 19:31:26 -0700

    Updated about page description

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ const About = () => {
 			<h2>About Me</h2>
             <br/><br/>
             <div className='container'>
-            <p className='text'>I am a Software Engineer with over 4 years of programming experience. Currently I am a senior in Computer Science at Portland State University. In 2022 I graduated from Lane Community College with an AAS in Computer Programming. During the final term of that degree I worked as a Web Development Intern at OCDLA, the Oregon Criminal Defense Lawyers Association.
+            <p className='text'>I'm a Software Engineer with over 5 years of programming experience and a strong foundation in both front-end and back-end development. In June 2025, I earned a Bachelor of Science in Computer Science from Portland State University, where I maintained a 3.74 major GPA (3.54 overall) and completed a team-based capstone project with PSU's BikePed Portal, developing data upload tools and automation pipelines to support transportation research.
             <br/><br/><br/>
             <a className='links' href='/pdfs/AlanShirkResume.pdf' target='_blank'>Click here to view my resumé!</a></p>
             </div>
